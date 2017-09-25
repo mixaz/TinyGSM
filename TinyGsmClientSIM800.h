a6aaa8f367ef5b4d232e4e41402b0f765177a61a
Dawid Cieszynski <dawid.cieszynski@crystalcode.pl>
2017-09-25 12:27:28 +0200

    deleteSMSOpt

--- TinyGsmClientSIM800.h
+++ TinyGsmClientSIM800.h
@@ -744,6 +744,20 @@ public:
 
     return true;
   }
+String deleteSMSOpt() {
+    sendAT(GF("+CMGD=?"));
+      if (waitResponse() != 1) {
+        return "";
+      }
+      if (waitResponse(10000L, GF(GSM_NL "+CMGD::")) != 1) {
+        return "";
+      }
+        stream.readStringUntil('"');
+        String indexes = stream.readStringUntil('"');
+        stream.readStringUntil(',');
+        String options = stream.readStringUntil('\n');
+        return indexes;
+      }
 
   bool readSMS(uint8_t i, String& msg){
     sendAT(GF("+CMGF=1"));
